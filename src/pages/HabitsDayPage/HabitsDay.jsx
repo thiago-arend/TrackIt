5530a2ff5f2c3c8ae873474006b22f997d129701
thiago-arend <thiago.arend@gmail.com>
2023-06-05 14:25:57 -0300

    finalização layout tela habitos dia

--- src/pages/HabitsDayPage/HabitsDay.jsx
+++ src/pages/HabitsDayPage/HabitsDay.jsx
@@ -1,17 +1,33 @@
 import { useLocation } from "react-router-dom";
-import { HabitsDayContainer } from "./styled";
+import { HabitsDayContainer, HabitContainer, Legenda } from "./styled";
+import check from "../../assets/check.svg"
 
 export default function HabitsDay() {
     const location = useLocation();
-    const {day, habits} = location.state;
+    const { day, habits } = location.state;
     console.log(habits);
 
     return (
         <HabitsDayContainer>
-            <h1>{day}</h1>
+            <h1>Hábitos de {day}</h1>
             {
-                habits.map(h => <p>{h.name}</p>)
+                habits.map(h => (
+                    <HabitContainer done={h.done}>
+                        <span>{h.name}</span>
+                        <span>{h.highest}</span>
+                    </HabitContainer>)
+                )
             }
+            <Legenda>
+                <div>
+                    <div></div>
+                    <div><span>Completou!</span><ion-icon name="happy-outline"></ion-icon></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div><span>Não completou...</span><ion-icon name="sad-outline"></ion-icon></div>
+                </div>
+            </Legenda>
         </HabitsDayContainer>
     )
 }
--- src/pages/HabitsDayPage/styled.js
+++ src/pages/HabitsDayPage/styled.js
@@ -4,4 +4,75 @@ export const HabitsDayContainer = styled.div`
     margin: 90px auto;
     padding: 0 18px;
     width: 340px;
+
+    h1 {
+        font-family: 'Lexend Deca';
+        font-weight: 400;
+        margin-bottom: 24px;
+        font-size: 23px;
+        color: #126BA5;
+    }
+`;
+
+export const HabitContainer = styled.div`
+    margin: 0 auto 16px;
+    padding: 12px 18px 12px;
+    width: 100%;
+    height: 50px;
+    background-color: ${props => props.done ? "#8FC549" : "#F79327"};
+    border: 1px solid ${props => props.done ? "#E7E7E7" : "#E7E7E7"};
+    border-radius: 5px;
+
+    font-family: "Lexend Deca";
+    font-weight: 400;
+    font-size: 14px;
+`;
+
+export const Legenda = styled.div`
+    width: 100%;
+    height: 30px;
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+
+    > div {
+        width: 150px;
+
+        &:nth-child(1) {
+            width: 116px;
+            
+            div {
+                ion-icon {
+                    color: #8FC549;
+                }
+            }
+        }
+
+        &:nth-child(2) {
+            
+            div {
+                ion-icon {
+                    color: #F79327;
+                }
+            }
+        }
+
+        > div {
+            &:nth-child(2) {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                font-family: "Lexend Deca";
+                font-weight: 400;
+                font-size: 14px;
+
+                ion-icon {
+                    width: 25px;
+                    height: 25px;
+                }
+            }
+        }
+    }
 `;
